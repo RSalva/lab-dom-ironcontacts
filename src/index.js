5a58aeca0860bc77cfd9d70c1b26d00e1b3a362f
Ramon Salva Sanchez <ramonsalvasanchez@gmail.com>
2025-04-15 21:11:36 +0200

    feed: completed iterations 1, 2 and 3

--- src/index.js
+++ src/index.js
@@ -36,22 +36,56 @@ tableBody.appendChild(exampleRow);
 
 // ITERATION 1 - Display 3 contacts
 // Get the first 3 contacts from the 'contacts' array.
+// console.log(contacts);
 const threeContacts = contacts.splice(0, 3);
+// console.log(threeContacts);
 
 // Your code goes here ...
-
+for (contact of threeContacts) {
+  const row = document.createElement("tr");
+  row.innerHTML = `
+    <td>
+      <img src="${contact.pictureUrl}" />
+    </td>
+    <td> ${contact.name} </td>
+    <td> ${contact.popularity.toFixed(2)} </td>
+    <td>
+      <button class="btn-delete">Delete</button>
+    </td>
+    <td>
+      <button class="btn-like">
+        <img src="./images/icon.png" alt="like" />
+      </button>
+    </td>
+  `;
+  deleteButton(row);
+  likeButton(row);
+  tableBody.appendChild(row);
+}
 
   
   // ITERATION 2 - Delete Buttons
   
   // Your code goes here ...
+  function deleteButton(row) {
+    const deleteButton = row.querySelector(".btn-delete");
+    deleteButton.addEventListener("click", function() {
+      row.remove();
+    });
+  }
+
   
   
 
   // ITERATION 3 - Like Buttons
 
   // Your code goes here ...
-
+  function likeButton(row) {
+    const likeButton = row.querySelector(".btn-like");
+    likeButton.addEventListener("click", function() {
+      this.classList.toggle("selected");
+    })
+  }
   
   
 
